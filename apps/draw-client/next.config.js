b9f5b5fec85e5d706fba3e6232f65071d19452a9
krishsinghhura <krishsinghhura@gmail.com>
2025-09-17 00:32:28 +0530

    fixed config

--- apps/draw-client/next.config.js
+++ apps/draw-client/next.config.js
@@ -1,15 +1,10 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: { unoptimized: true },
-  experimental: {
-    esmExternals: 'loose'
-  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
